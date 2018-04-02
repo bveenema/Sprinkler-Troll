81566b3b459ab7be115245b26bb818d3f9173128
Veen <ben@veenemadesignworks.com>
2018-04-02 05:29:58 -0400

    Project Creation

---
+++ src/Sprinkler-Troll.ino
@@ -0,0 +1,17 @@
+/*
+ * Project Sprinkler-Troll
+ * Description: Sprinkler Controller
+ * Author: Veenema Design Works
+ */
+
+// setup() runs once, when the device is first turned on.
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
