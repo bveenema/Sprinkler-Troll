9c230ad3ff0b508284b182b0cb502a92a24e059c
unknown <bveenema@gmail.com>
2018-04-09 04:58:25 -0400

    Made config multi-callable

--- src/config.h
+++ src/config.h
@@ -1,20 +1,28 @@
+#include "application.h"
+
+#ifndef SPRINKLER_CONFIG_H
+#define SPRINKLER_CONFIG_H 1
+
+
 const pin_t SWITCH_PIN = D3;
+const pin_t SPRINKLER_RELAY = D0;
 
 
-uint8_t statsAddr = 0;
+const uint8_t statsAddr = 0;
+
 struct Stats {
   uint8_t version; // 0
   uint32_t duration; // seconds
-  uint32_t deadline; // unix time (sunrise)
-  uint32_t targetStartTime; // unix time
+  int32_t deadline; // unix time (sunrise)
+  int32_t targetStartTime; // unix time
   uint32_t cityID; // Open Weather API city ID
 };
-Stats SprinklerStats;
 
-Stats defaultStats = { // 15 minutes, 4/6/18 @ 6:30am EST, 4/6/18 @ 6:15am EST, Claremont NH
-                      0,
-                      900,
-                      1522996200,
-                      1522995300,
-                      5084633
+const Stats defaultStats = {
+                      0, // Version 0
+                      900, // 15 minutes
+                      1522996200, // 4/6/18 @ 6:30am EST
+                      1522995300, //4/6/18 @ 6:15am EST
+                      5084633 //Claremont NH
                     };
+#endif
